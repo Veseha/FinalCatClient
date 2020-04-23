1983537bf0324a9db430b074c6ecf01f63cf0cff
Veseha <sveula@list.ru>
2020-04-23 22:20:19 +0600

    Update flask sqlalchemy

--- FLASK-SQLALCHEMY #1/Veseha/main.py
+++ FLASK-SQLALCHEMY #1/Veseha/main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from data import db_session
-from .data.users import User
-
+from data.users import User
 
 
 app = Flask(__name__)
@@ -10,14 +9,18 @@ db_session.global_init("db/blogs.sqlite")
 
 
 def main():
-    app.run()
     user = User()
     user.name = "Пользователь 1"
     user.about = "биография пользователя 1"
     user.email = "email@email.ru"
+    print(user)
+    print('----------------------------------------')
     session = db_session.create_session()
     session.add(user)
     session.commit()
+
+    app.run()
+
 
 
 if __name__ == '__main__':
