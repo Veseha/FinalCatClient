3ea9ba91eceabc9e7c12fcc9ca2e45ed951704b2
Veseha <sveula@list.ru>
2020-05-03 21:36:30 +0600

    Fix - bd not init - fix - bd work stability

---
+++ REST-API/Veseha/base/data/news_resources.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, redirect, request, make_response, session, abort, Blueprint, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,  BooleanField, SubmitField, TextAreaField,\
+    SubmitField, ValidationError, TextField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from data import db_session
+from data.users import User
+from data.news import News
+import news_api
+from flask_restful import reqparse, abort, Api, Resource
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import datetime
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True)
+parser.add_argument('content', required=True)
+parser.add_argument('is_private', required=True, type=bool)
+parser.add_argument('is_published', required=True, type=bool)
+parser.add_argument('user_id', required=True, type=int)
+
+
+class NewsResource(Resource):
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        return jsonify({'news': news.to_dict(
+            only=('title', 'content', 'user_id', 'is_private'))})
+
+    def delete(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(News).get(news_id)
+        session.delete(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(News).all()
+        return jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_published=args['is_published'],
+            is_private=args['is_private']
+        )
+        session.add(news)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(News).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
